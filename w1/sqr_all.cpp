5add8bfba9556c2f10e29df0795a42f0b9a99437
MSM <nikolayenkoserhiy@gmail.com>
2020-07-09 07:53:15 +0300

    sqr_all

--- w1/sqr_all.cpp
+++ w1/sqr_all.cpp
@@ -6,25 +6,71 @@
 using namespace std;
 
 // Предварительное объявление шаблонных функций
-template<typename T> T FuncA(T x);
-template<typename T> void FuncB(T x);
 
+template <typename First, typename Second>
+pair<First, Second> operator * (const pair<First, Second>& p1, const pair<First, Second>& p2);
+
+template <typename T>
+vector<T> operator * (const vector<T>& vector1);
+
+template <typename Key, typename Value>
+map<Key, Value> operator * (const map<Key, Value>& dict);
 // Объявляем шаблонные функции
-template <typename T>
-T FuncA(T x) { /*...*/ }
+
+template <typename Collection>
+Collection Sqr(const Collection& col) {
+    Collection res;
+    for (const auto& item : col) {
+        item * item;
+    }
+    return res;
+}
+
+template <typename First, typename Second>
+pair<First, Second> operator * (const pair<First, Second>& p1, const pair<First, Second>& p2) {
+    First f = p1.first * p2.first;
+    Second s = p2.second * p2.second;
+    return {f, s};
+}
 
 template <typename T>
-void FuncB(T x) { /*...*/ }
+vector<T> operator * (const vector<T>& vector1) {
+    vector<T> res;
+    for (const auto& x : vector1) {
+        res.push_back(x * x);
+    }
+}
+template <typename Key, typename Value>
+map<Key, Value> operator * (const map<Key, Value>& dict) {
+    map<Key, Value> res;
+    for (auto [key, value] : dict) {
+        res[key] = value * value;
+    }
+}
+
+
+
+//template <typename T> // ключевое слово для объявления типа T
+//T Sqr(T x) {
+//    return x * x;
+//     нам нужно, чтобы элемент x поддерживал операцию умножения
+//}
 
 int main () {
 
+//    cout << Sqr(3.4) << endl;
+//
+    cout << "===================="/**/ << endl;
+    auto p = make_pair(3, 4);
+    auto res = Sqr(p);
+    cout << res.first << " " << res.second << endl;
     vector<int> v = {1, 2, 3};
     cout << "vector:";
     for (int x : Sqr(v)) {
         cout << ' ' << x;
     }
     cout << endl;
-
+/*
     map<int, pair<int, int>> map_of_pairs = {
             {4, {2, 2}},
             {7, {4, 3}}
@@ -33,6 +79,6 @@ int main () {
     for (const auto& x : Sqr(map_of_pairs)) {
         cout << x.first << ' ' << x.second.first << ' ' << x.second.second << endl;
     }
-
+*/
     return 0;
 }
